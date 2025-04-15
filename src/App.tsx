87be0f1eb67dd48c5dc6b704fb1a48575fbdd236
mehrnaz <ghaffar@citynet.ir>
2025-04-15 17:20:34 +0330

    chore:add Search

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Title from "./components/title/Title";
+import Search from "./components/search/Search";
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <div className="container">
       <Title />
-    </>
+      <Search />
+    </div>
   );
 }
 
---
+++ src/components/search/Search.tsx
@@ -0,0 +1,11 @@
+import TextField from "@mui/material/TextField";
+
+function Search() {
+  return (
+    <>
+      <TextField label="Location" className="" />
+    </>
+  );
+}
+
+export default Search;
