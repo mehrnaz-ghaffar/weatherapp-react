219fe37d2be5026688f1db41745ac7d8262426ba
mehrnaz <ghaffar@citynet.ir>
2025-04-19 17:34:00 +0330

    style:add some styles

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,8 +1,10 @@
 import TextField from "@mui/material/TextField";
 import { useEffect, useState } from "react";
 import useAxios from "../../hooks/useAxios";
+import "./Search.css";
 
 function Search() {
+  const [apiKey, setApiKey] = useState("d7b29e486645423084d124534251504");
   const [searchKey, setSearchKey] = useState("");
   // const [data, setData] = useState({});
   const {
@@ -11,14 +13,13 @@ function Search() {
     loading,
     refetch,
   } = useAxios(
-    `http://api.weatherapi.com/v1/current.json?key=d7b29e486645423084d124534251504&q=${searchKey}&aqi=no`,
+    `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${searchKey}&aqi=no`,
     "post"
   );
 
   console.log("data", data);
   console.log("error", error);
   console.log("loading", loading);
-  console.log("refetch", refetch);
 
   useEffect(() => {
     if (searchKey.length > 3) {
@@ -32,7 +33,8 @@ function Search() {
       <TextField
         value={searchKey}
         label="Location"
-        className=""
+        className="inputClass"
+        variant="outlined"
         onChange={(event) => {
           setSearchKey(event.target.value);
         }}
